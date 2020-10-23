80c56a5c4638db6be7a0aa05a5db495e3e6a104c
cooljasonmelton <cooljasonmelton>
2020-10-23 01:24:09 -0500

    square colors set up

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,9 +6,21 @@ import './Board.css';
 const Board = () => {
     const boardArr = [...new Array(64)]
 
+    const isEven = n => n % 2 === 0;
+     
+    const squareClass = index => {
+        const classArr = ["cfb"]
+        // rank even, file odd OR rank odd, file even --> dark square
+        const rank = isEven(Math.floor(index/8))
+        const file = isEven(index % 8)
+        if ((rank && !file) || (!rank && file)) classArr.push("dark")
+
+        return classArr.join(" ")
+    };
+
     return (
         <div className="Board">
-            {boardArr.map((sq, i) => <div key={i} className="square cfb"> sq </div>)}
+            {boardArr.map((sq, i) => <div key={i} className={squareClass(i)}> </div>)}
         </div>
     );
 }
