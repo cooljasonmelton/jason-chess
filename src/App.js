5d5ffe9b3cc6017176e89f37a465d9eb43d7670b
cooljasonmelton <cooljasonmelton>
2020-10-26 06:36:48 -0500

    working on pawn functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // styling
 import './App.css';
@@ -8,9 +8,9 @@ import Navbar from './components/navbar/Navbar'
 import Board from './components/board/Board'
 
 const App = () => {
-
   // later move this to redux or something to make it available to whole app
-  const boardArr = [
+  const [board, setBoard] = useState(
+    [
       ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
       ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
       [null, null, null, null, null, null, null, null],
@@ -19,13 +19,21 @@ const App = () => {
       [null, null, null, null, null, null, null, null],
       ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
       ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
-  ]
+    ]
+  )
+  // if true, white's turn
+  const [turn, setTurn] = useState(true)
+
   return (
     <div className="App">
       <Navbar/>
 
       <div className="cfb">
-        <Board board={boardArr}/>
+        <Board 
+          board={board}
+          setBoard={setBoard}
+          turn={turn}
+          setTurn={setTurn}/>
       </div>
 
     </div>
---
+++ src/components/pieces/pawn/Pawn.js
@@ -0,0 +1,3 @@
+export const clickPawn = () => {
+    console.log('pawn')
+}
